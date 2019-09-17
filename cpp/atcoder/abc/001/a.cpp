316dbd405a08f1e21719fb7895aa914e5cb9923e
conao3 <conao3@gmail.com>
2019-09-17 20:23:37 +0900

    add cpp

---
+++ cpp/atcoder/abc/001/a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  std::cout << a - b << std::endl;
+  return 0;
+}
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
+asdf
