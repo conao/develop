8af5d457e98aa48de593155f7c60b4d9c394468d
conao3 <conao3@gmail.com>
2019-09-19 22:39:27 +0900

    add sample xcode project

---
+++ cpp/sample/sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sample
+//
+//  Created by Naoya Yamashita on 2019/09/19.
+//  Copyright © 2019年 Naoya Yamashita. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
