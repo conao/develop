fefa8c16421873b5a703b77de79251002c51974a
conao3 <conao3@gmail.com>
2019-12-03 23:03:45 +0900

    fix leaf/init.el

--- el/.debug.emacs.d/leaf/init.el
+++ el/.debug.emacs.d/leaf/init.el
@@ -1,3 +1,11 @@
+;;; init.el --- leaf bytecompile sample   -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; leaf bytecompile sample
+
+;;; Code:
+
 ;; ~/.debug.emacs.d/leaf/init.el
 
 ;; you can run like 'emacs -q -l ~/.debug.emacs.d/{{pkg}}/init.el'
@@ -5,32 +13,56 @@
   (setq user-emacs-directory
         (expand-file-name (file-name-directory load-file-name))))
 
-(prog1 "prepare leaf"
-  (prog1 "package"
+(prog1 "leaf"
+  (prog1 "install leaf"
     (custom-set-variables
      '(package-archives '(("org"   . "https://orgmode.org/elpa/")
                           ("melpa" . "https://melpa.org/packages/")
                           ("gnu"   . "https://elpa.gnu.org/packages/"))))
-    (package-initialize))
-
-  (prog1 "leaf"
+    (package-initialize)
     (unless (package-installed-p 'leaf)
       (package-refresh-contents)
-      (package-install 'leaf))))
+      (package-install 'leaf)))
+
+  (leaf leaf-keywords
+    :ensure t
+    :config
+    ;; optional packages if you want to use :hydra, :el-get,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t
+      :custom ((el-get-git-shallow-clone . t)))
+
+    ;; initialize leaf-keywords.el
+    (leaf-keywords-init)))
 
 
 
 (load (locate-user-emacs-file "../essentials.el"))
 
-(leaf leaf-keywords
-  :init
-  (leaf hydra :ensure t)
-  :config
-  (leaf-keywords-init))
+(leaf magit
+  :when (version<= "25.1" emacs-version)
+  :ensure t
+  :preface
+  (defun c/git-commit-a ()
+    "Commit after add anything."
+    (interactive)
+    (shell-command "git add .")
+    (magit-commit-create))
+  :bind (("M-=" . hydra-magit/body))
+  :hydra (hydra-magit
+          (:hint nil :exit t)
+          "
+^^         hydra-magit
+^^------------------------------
+ _s_   magit-status
+ _C_   magit-clone
+ _c_   magit-commit
+ _d_   magit-diff-working-tree
+ _M-=_ magit-commit-create"
+          ("s" magit-status)
+          ("C" magit-clone)
+          ("c" magit-commit)
+          ("d" magit-diff-working-tree)
+          ("M-=" c/git-commit-a)))
 
-(leaf face-remap
-  :hydra (hydra-zoom
-          (global-map "<f2>")
-          "zoom"
-          ("g" text-scale-increase "in")
-          ("l" text-scale-decrease "out")))
+;;; init.el ends here
