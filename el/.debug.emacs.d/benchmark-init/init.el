2439fcda6590dd17de353dc3da4dee7ecb2f19bb
conao3 <conao3@gmail.com>
2019-12-09 12:16:50 +0900

    add benchmark-init/init.el

---
+++ el/.debug.emacs.d/benchmark-init/init.el
@@ -0,0 +1,33 @@
+;; ~/.debug.emacs.d/benchmark-init/init.el
+
+;; you can run like 'emacs -q -l ~/.debug.emacs.d/{{pkg}}/init.el'
+(when load-file-name
+  (setq user-emacs-directory
+        (expand-file-name (file-name-directory load-file-name))))
+
+(prog1 "prepare leaf"
+  (prog1 "package"
+    (custom-set-variables
+     '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+                          ("melpa" . "https://melpa.org/packages/")
+                          ("gnu"   . "https://elpa.gnu.org/packages/"))))
+    (package-initialize))
+
+  (prog1 "leaf"
+    (unless (package-installed-p 'leaf)
+      (package-refresh-contents)
+      (package-install 'leaf))))
+
+
+
+(load (locate-user-emacs-file "../essentials.el"))
+
+(leaf benchmark-init
+  :leaf-defer nil
+  :ensure t
+  :require t
+  :hook ((after-init-hook . benchmark-init/deactivate)))
+
+(leaf lsp-mode
+  :ensure t
+  :require t)
