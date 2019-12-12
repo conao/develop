f5d1bd5e0b839af1314f9a95ee134315d502e535
conao3 <conao3@gmail.com>
2019-12-13 02:56:03 +0900

    fix solarized theme init.el

--- el/.debug.emacs.d/solarized-theme/init.el
+++ el/.debug.emacs.d/solarized-theme/init.el
@@ -21,43 +21,47 @@
 (load (locate-user-emacs-file "../essentials.el"))
 
 (leaf solarized-theme
-    :ensure t
-    :when (version<= "24.1" emacs-version)
-    :custom ((solarized-distinct-fringe-background . t))
-    :defun (solarized-create-theme-file-with-palette solarized-color-blend)
-    :require t
-    :preface
-    (defun c/reload-theme ()
-      (interactive)
-      (let ((theme-name (save-excursion
-                          (goto-char (point-min))
-                          (re-search-forward "deftheme \\([^ ]*\\)")
-                          (substring-no-properties (match-string 1)))))
-        (eval-buffer)
-        (load-theme (intern theme-name) t)))
-    :config
-    
-    (solarized-create-theme-file-with-palette 'dark 'solarized-zenburn-dark
-      '("#3F3F3F" "#FFFFFF"           ; base03 (02 01 00 0 1 2) base3
-        "#b58900" "#cb4b16"           ; yellow orange
-        "#dc322f" "#d33682"           ; red    magenta
-        "#6c71c4" "#268bd2"           ; violet blue
-        "#2aa198" "#F0DFAF"           ; cyan   green
-        )
-      '((custom-theme-set-faces
-         theme-name
-         ;; `(default ((,class (:foreground ,base2 :background ,base03))))
-         ;; `(vertical-border ((,class (:foreground ,base03))))
-         ;; `(mode-line ((,class (:foreground ,base2 :background ,base02))))
-         ;; `(mode-line-inactive ((,class (:foreground ,base0 :background ,base03))))
-         ;; `(font-lock-builtin-face ((,class (:foreground ,magenta))))
-         ))
-      t)
-    ;; (solarized-create-theme-file-with-palette 'dark 'solarized-zenburn-dark
-    ;;     '("#3F3F3F" "#FFFFFD"           ; base03 (02 01 00 0 1 2) base3
-    ;;       "#F0DFAF" "#DFAF8F"           ; yellow orange
-    ;;       "#CC9393" "#DC8CC3"           ; red    magenta
-    ;;       "#6C3333" "#8CD0D3"           ; violet blue
-    ;;       "#93E0E3" "#7F9F7F"           ; cyan   green
-    ;;       ))
-    (load-theme 'solarized-zenburn-dark t))
+  ;; :ensure t
+  :when (version<= "24.1" emacs-version)
+  :load-path `,(locate-user-emacs-file "site-lisp/solarized-theme")
+  :custom ((solarized-distinct-fringe-background . t))
+  :defun (solarized-create-theme-file-with-palette solarized-color-blend)
+  :require t
+  :preface
+  (defun c/reload-theme ()
+    (interactive)
+    (let ((theme-name (save-excursion
+                        (goto-char (point-min))
+                        (re-search-forward "deftheme \\([^ ]*\\)")
+                        (substring-no-properties (match-string 1)))))
+      (eval-buffer)
+      (load-theme (intern theme-name) t)))
+  :config
+  
+  (solarized-create-theme-file-with-palette 'dark 'solarized-zenburn-dark
+    '("#3F3F3F" "#FFFFFF"           ; base03 (02 01 00 0 1 2) base3
+      "#b58900" "#cb4b16"           ; yellow orange
+      "#dc322f" "#d33682"           ; red    magenta
+      "#6c71c4" "#268bd2"           ; violet blue
+      "#2aa198" "#F0DFAF"           ; cyan   green
+      )
+    '((custom-theme-set-faces
+       theme-name
+       ;; `(default ((,class (:foreground ,base2 :background ,base03))))
+       ;; `(vertical-border ((,class (:foreground ,base03))))
+       ;; `(mode-line ((,class (:foreground ,base2 :background ,base02))))
+       ;; `(mode-line-inactive ((,class (:foreground ,base0 :background ,base03))))
+       ;; `(font-lock-builtin-face ((,class (:foreground ,magenta))))
+       ))
+    t)
+  ;; (solarized-create-theme-file-with-palette 'dark 'solarized-zenburn-dark
+  ;;     '("#3F3F3F" "#FFFFFD"           ; base03 (02 01 00 0 1 2) base3
+  ;;       "#F0DFAF" "#DFAF8F"           ; yellow orange
+  ;;       "#CC9393" "#DC8CC3"           ; red    magenta
+  ;;       "#6C3333" "#8CD0D3"           ; violet blue
+  ;;       "#93E0E3" "#7F9F7F"           ; cyan   green
+  ;;       ))
+  (load-theme 'solarized-zenburn-dark t))
+
+(leaf highlight-symbol
+  :ensure t :require t)
