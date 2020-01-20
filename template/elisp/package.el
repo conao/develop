19adccff1136aa358f933b6ba40352d223e61430
conao3 <conao3@gmail.com>
2020-01-20 19:55:27 +0900

    regenerate elisp template files

---
+++ template/elisp/package.el
@@ -0,0 +1,43 @@
+;;; package.el ---   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Version: 0.0.1
+;; Keywords: tools
+;; Package-Requires: ((emacs "25.1"))
+;; URL: https://github.com/conao3/package.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+
+;;; Code:
+
+(defgroup package nil
+  ""
+  :prefix "package-"
+  :group 'tools
+  :link '(url-link :tag "Github" "https://github.com/conao3/package.el"))
+
+(provide 'package)
+
+;; Local Variables:
+;; indent-tabs-mode: nil
+;; End:
+
+;;; package.el ends here
